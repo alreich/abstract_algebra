3a5d3efd8eac110e0beb9c905ce1782a3488e132
Al Reich <al.reich@gmail.com>
2021-05-21 12:03:47 -0500

    Lowercases dirs

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../Source/'))
+sys.path.insert(0, os.path.abspath('../src/'))
 
 # -- Project information -----------------------------------------------------
 

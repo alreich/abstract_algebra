9bea8c430d6c7ecc266d069319b382e34f753f03
Alfred Reich <al.reich@gmail.com>
2021-07-02 23:58:00 -0500

    WIP: new object hierarchy

---
+++ src/finite_algebras.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# New Object Hierarchy (WORK-IN-PROGRESS)
+
+import functools as fnc
+import numpy as np
+import table_utils
+
+
+# =========
+#   Magma
+# =========
+
+class Magma:
+    
+    def __init__(self, elems, tbl):
+        self.__elements = elems
+        self.__table = np.array(tbl)
+        
+    def __contains__(self, element):
+        return element in self.__elements
+
+    def __getitem__(self, index):
+        return self.__elements[index]
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(\n{self.__elements}, \n{self.__table}\n)"
+
+    @property
+    def elements(self):
+        return self.__elements
+    
+    def set_elements(self, new_elements):
+        if isinstance(new_elements, list):
+            self.__elements = new_elements
+        elif isinstance(new_elements, dict):
+            self.__elements = [new_elements[elem] for elem in self.__elements]
+        return self
+    
+    @property
+    def table(self):
+        return self.__table
+
+    def op(self, *args):
+        if len(args) == 1:
+            if args[0] in self.__elements:
+                return args[0]
+            else:
+                raise ValueError(f"{args[0]} is not a valid element name")
+        elif len(args) == 2:
+            row = self.__elements.index(args[0])
+            col = self.__elements.index(args[1])
+            index = self.__table[row, col]
+            return self.__elements[index]
+        else:
+            return fnc.reduce(lambda a, b: self.op(a, b), args)
+    
+    def table_with_names(self):
+        return [[self.__elements[index] for index in row] for row in self.__table]
+
+
+# =============
+#   Semigroup
+# =============
+
+class Semigroup(Magma):
+
+    def __init__(self, elems, tbl):
+        if table_utils.is_associative(tbl):
+            super().__init__(elems, tbl)
+        else:
+            raise ValueError("Table does not support associativity")
+
+
+# ==========
+#   Monoid
+# ==========
+
+class Monoid(Semigroup):
+
+    def __init__(self, elems, tbl):
+        self.identity = table_utils.has_identity(tbl)
+        if self.identity:
+            super().__init__(elems, tbl)
+        else:
+            raise ValueError("Table has no identity element")
+
+
+if __name__ == '__main__':
+
+    print("\n=======================================================================")
+
+    print("\n--------------")
+    print("START OF TESTS")
+    print("--------------")
+
+    # Magma tests
+
+    print("\nMagma Tests:\n")
+
+    # Rock-Paper-Scissors Magma
+    rps = Magma(['r', 'p', 's'], [[0, 1, 0], [1, 1, 2], [0, 2, 2]])
+    print(rps)
+    ps = rps.op('p', 's')
+    rp = rps.op('r', 'p')
+    r_ps = rps.op('r', ps)
+    rp_s = rps.op(rp, 's')
+    print(f"    r(ps) = r{ps} = {r_ps}, \nbut (rp)s = {rp}s = {rp_s}")
+
+    print("\nSemigroup Tests:\n")
+
+    ex141_tbl = [[0, 3, 0, 3, 0, 3], [1, 4, 1, 4, 1, 4], [2, 5, 2, 5, 2, 5],
+                 [3, 0, 3, 0, 3, 0], [4, 1, 4, 1, 4, 1], [5, 2, 5, 2, 5, 2]]
+
+    ex141_sg = Semigroup(['a', 'b', 'c', 'd', 'e', 'f'], ex141_tbl)
+    print(ex141_sg)
+    print(f"Commutative?: {table_utils.is_commutative(ex141_sg.table)}")
+
+    print("\n------------")
+    print("END OF TESTS")
+    print("------------")
---
+++ src/table_utils.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Table Utilities
+
+def is_associative(table):
+    indices = range(len(table))
+    result = True
+    for a in indices:
+        for b in indices:
+            for c in indices:
+                ab = table[a][b]
+                bc = table[b][c]
+                if not (table[ab][c] == table[a][bc]):
+                    result = False
+                    break
+    return result
+
+
+def is_commutative(table):
+    indices = range(len(table))
+    result = True
+    for a in indices:
+        for b in indices:
+            if table[a][b] != table[b][a]:
+                result = False
+                break
+    return result
+
+
+def has_left_identity(table):
+    indices = range(len(table))
+    identity = None
+    for x in indices:
+        if all(table[x][y] == y for y in indices):
+            identity = x
+            break
+    return identity
+
+
+def has_right_identity(table):
+    indices = range(len(table))
+    identity = None
+    for x in indices:
+        if all(table[y][x] == y for y in indices):
+            identity = x
+            break
+    return identity
+
+
+if __name__ == '__main__':
+
+    print("\n=======================================================================")
+
+    print("\n--------------")
+    print("START OF TESTS")
+    print("--------------")
+
+    import pprint as pp
+
+    # Table Tests
+
+    print("\nTable Tests:\n")
+
+    # not assoc; is comm; no identity -- the RPS magma table, above
+    tbl1 = [[0, 1, 0], [1, 1, 2], [0, 2, 2]]
+
+    # is assoc; not comm; has identity (0) --- the S3 group table
+    tbl2 = [[0, 1, 2, 3, 4, 5], [1, 2, 0, 5, 3, 4], [2, 0, 1, 4, 5, 3],
+            [3, 4, 5, 0, 1, 2], [4, 5, 3, 2, 0, 1], [5, 3, 4, 1, 2, 0]]
+
+    # is assoc; is comm; has identity (0) --- the Z4 group table
+    tbl3 = [[0, 1, 2, 3], [1, 2, 3, 0], [2, 3, 0, 1], [3, 0, 1, 2]]
+
+    # powerset(3) group table
+    tbl4 = [[0, 1, 2, 3, 4, 5, 6, 7], [1, 0, 4, 5, 2, 3, 7, 6], [2, 4, 0, 6, 1, 7, 3, 5],
+            [3, 5, 6, 0, 7, 1, 2, 4], [4, 2, 1, 7, 0, 6, 5, 3], [5, 3, 7, 1, 6, 0, 4, 2],
+            [6, 7, 3, 2, 5, 4, 0, 1], [7, 6, 5, 4, 3, 2, 1, 0]]
+
+    tbl5 = [[0, 3, 0, 3, 0, 3], [1, 4, 1, 4, 1, 4], [2, 5, 2, 5, 2, 5],
+            [3, 0, 3, 0, 3, 0], [4, 1, 4, 1, 4, 1], [5, 2, 5, 2, 5, 2]]
+
+    test_tables = [tbl1, tbl2, tbl3, tbl4, tbl5]
+
+    for tbl in test_tables:
+        pp.pprint(tbl)
+        print()
+
+    print("   Table     Associative?  Commutative?   Left Id?   Right Id?")
+    print('-' * 65)
+    for tbl in test_tables:
+        i = test_tables.index(tbl) + 1
+        is_assoc = str(is_associative(tbl))
+        is_comm = str(is_commutative(tbl))
+        lft_id = str(has_left_identity(tbl))
+        rgt_id = str(has_right_identity(tbl))
+        print(f"{i :>{6}} {is_assoc :>{14}} {is_comm :>{12}} {lft_id :>{12}} {rgt_id :>{12}}")
+
+    print("\n------------")
+    print("END OF TESTS")
+    print("------------")

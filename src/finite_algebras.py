a1a7815d2c99c40b9215d7cba370f4bc2a146a09
Alfred Reich <al.reich@gmail.com>
2021-07-10 18:45:25 -0500

    working on finite algebra maker; added new magmas

--- src/finite_algebras.py
+++ src/finite_algebras.py
@@ -5,6 +5,7 @@
 
 # New Object Hierarchy (WORK-IN-PROGRESS)
 
+import json
 import functools as fnc
 from cayley_table import CayleyTable
 
@@ -26,10 +27,12 @@ class Magma:
         self.__elements = elements
         self.__table = CayleyTable(table)
         self.metadata = metadata  # dictionary with, say, name, description, etc.
-        self.__is_associative = None  # Cached on first access
-        self.__is_commutative = None  # Cached on first access
+        # The following values, if they exist, are cached on first access
+        self.__is_associative = None
+        self.__is_commutative = None
         self.__has_identity = None
         self.__identity = None
+        self.__inverses = None
 
     def __eq__(self, other):
         if self.__elements == other.elements:  # Same elements in the same order
@@ -131,6 +134,78 @@ class Group(Monoid):
         super().__init__(elems, tbl)
         if not self.table.has_inverses():
             raise ValueError("Table has insufficient inverses")
+
+
+# ====================
+# Finite Algebra Maker
+# ====================
+
+def finite_algebra_maker(*args):
+
+    if len(args) == 1:
+
+        # Create from a JSON file
+        if isinstance(args[0], str):
+            with open(args[0], 'r') as fin:
+                finalg_dict = json.load(fin)
+
+        # Create from a dictionary
+        elif isinstance(args[0], dict):
+            finalg_dict = args[0]
+
+        # Create from a list of lists of element names (str)
+        elif isinstance(args[0], list):
+            finalg_dict = {'name': "no name",
+                           'description': "Constructed from multiplication table",
+                           'elements': args[0][0],
+                           'table': args[0]
+                           }
+
+        # No other type of single argument can create a Finite Algebra
+        else:
+            raise Exception("Single argument must be a string or a dictionary.")
+
+    # If 3 args, then they are: name, description, & table element names (list of lists of str)
+    elif len(args) == 3:
+        finalg_dict = {'name': args[0],
+                       'description': args[1],
+                       'elements': args[2][0],  # top row of table input
+                       'table': index_table_from_name_table(args[2])
+                       }
+
+    # If 4 args, then they are: name, description, list of element names, & table (ints)
+    else:
+        # Assumes all four possible fields were input
+        finalg_dict = {'name': args[0],
+                       'description': args[1],
+                       'elements': args[2],
+                       'table': args[3]
+                       }
+
+    # Create the list of element names
+    if finalg_dict.get('elements') is None:
+        finalg_dict['elements'] = finalg_dict['table'][0]  # First row of table
+
+    # Create a CayleyTable object
+    tbl = finalg_dict['table']
+    if isinstance(tbl[0][0], str):
+        finalg_dict['table'] = index_table_from_name_table(tbl)
+
+    return finalg_dict
+
+
+def print_finite_algebra(finalg_dict):
+    nm = finalg_dict['name']
+    desc = finalg_dict['description']
+    elems = finalg_dict['element_names']
+    tbl = finalg_dict['cayley_table']
+    return f"FiniteAlgebra('{nm}',\n'{desc}',\n{elems},\n{tbl}) "
+
+
+def index_table_from_name_table(name_table):
+    top_row = name_table[0]
+    return [[top_row.index(elem_name) for elem_name in row] for row in name_table]
+
 
 
 if __name__ == '__main__':

ca8ba23cd12e15e5555485d27ddbfeb4046f38ed
Alfred Reich <al.reich@gmail.com>
2021-08-08 18:36:21 -0500

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='abstract_algebra',
+    version='0.0.1',
+    packages=['src', 'test'],
+    url='https://abstract-algebra.readthedocs.io/en/latest/index.html',
+    license='MIT',
+    author='Alfred J. Reich',
+    author_email='al.reich@gmail.com',
+    description='A Python implementation of Finite Algebras: Magma, Semigroup, Monoid, Group, Ring, and Field'
+)

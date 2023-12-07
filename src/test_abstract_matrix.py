9b0ac845d2b0a20c1e15060fb9d90cf69753f955
Alfred Reich <al.reich@gmail.com>
2023-12-06 18:06:49 -0600

    adding AbstractMatrix tests WIP

--- src/test_abstract_matrix.py
+++ src/test_abstract_matrix.py
@@ -1,19 +1,45 @@
 from unittest import TestCase
-
+import finite_algebras as alg
+import abstract_matrix as mat
+import numpy as np
 
 class TestAbstractMatrix(TestCase):
 
     def setUp(self) -> None:
-        pass
 
-    # def test_zeros(self):
-    #     self.fail()
+        # The following duplication is for equality testing
+        self.psr3 = alg.generate_powerset_ring(3)
+        self.psr3x = alg.generate_powerset_ring(3)
+        self.arr1 = [['{0, 1, 2}', '{2}', '{2}'],
+                     ['{0, 1, 2}', '{2}', '{1, 2}'],
+                     ['{0}', '{0, 1, 2}', '{0, 1, 2}']]
+        self.arr1x = [['{0, 1, 2}', '{2}', '{2}'],
+                      ['{0, 1, 2}', '{2}', '{1, 2}'],
+                      ['{0}', '{0, 1, 2}', '{0, 1, 2}']]
+        self.mat1 = mat.AbstractMatrix(self.arr1, self.psr3)
+        self.mat1x = mat.AbstractMatrix(self.arr1x, self.psr3x)
 
-    # def test_identity(self):
-    #     self.fail()
+    def test_equality_of_matrices(self):
+        self.assertTrue(self.mat1 == self.mat1x)
 
-    # def test_random(self):
-    #     self.fail()
+    def test_zeros(self):
+        self.assertEqual(mat.AbstractMatrix.zeros((3, 4), self.psr3).array.tolist(),
+                         [['{}', '{}', '{}', '{}'],
+                          ['{}', '{}', '{}', '{}'],
+                          ['{}', '{}', '{}', '{}']])
+
+    def test_identity(self):
+        self.assertEqual(mat.AbstractMatrix.identity(3, self.psr3).array.tolist(),
+                         [['{0, 1, 2}', '{}', '{}'],
+                          ['{}', '{0, 1, 2}', '{}'],
+                          ['{}', '{}', '{0, 1, 2}']])
+
+    def test_random(self):
+        np.random.seed(1)
+        self.rand = (mat.AbstractMatrix.random((3, 4), self.psr3).array.tolist(),
+                     [['{0, 2}', '{2}', '{0, 1}', '{}'],
+                      ['{0, 1, 2}', '{0}', '{2}', '{0, 2}'],
+                      ['{0, 1, 2}', '{}', '{}', '{0}']])
 
     # def test_array(self):
     #     self.fail()

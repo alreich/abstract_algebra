341fb9ae06a1b186a247f9825e34b15c254b22d6
Alfred Reich <al.reich@gmail.com>
2024-02-18 20:15:00 -0600

    Saving WIP

--- src/gaussian_integers.py
+++ src/gaussian_integers.py
@@ -10,13 +10,18 @@
 """
 
 # from my_math import divides
-import my_math as my
+# import my_math as my
+
+from typing import TypeVar
+
+C = TypeVar('C', int, float, complex)
+N = TypeVar('N', int, float)
 
 
 class Gint():
     """Gaussian Integer Class"""
 
-    def __init__(self, re=1, im=0):
+    def __init__(self, re: C = 1, im: N = 0):
 
         if isinstance(re, int):
             self.real = re
@@ -36,10 +41,10 @@ class Gint():
         else:
             raise ValueError(f"{im} cannot be used for the imaginary part of a Gint instance")
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Gint({self.real}, {self.imag})"
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(complex(self))
 
     def __add__(self, other):
@@ -76,21 +81,6 @@ class Gint():
         else:
             raise ValueError(f"Multiplication by '{other}' not supported")
 
-    def __truediv__(self, other):
-        """If other divides self, then self / other is returned,
-        otherwise False is returned.
-        """
-        if isinstance(other, int):
-            oth = Gint(other, 0)
-        else:
-            oth = other
-        numer = self * oth.conj
-        denom = oth.norm
-        if my.divides(denom, numer.real) and my.divides(denom, numer.imag):
-            return Gint(numer.real // denom, numer.imag // denom)
-        else:
-            return False
-
     def __pow__(self, n, modulo=None):
         result = self
         if isinstance(n, int) and n >= 0:
@@ -115,6 +105,27 @@ class Gint():
     def __ne__(self, other):
         return (self.real != other.real) or (self.imag != other.imag)
 
+    def __truediv__(self, other):  # self / other
+        """Return the exact, complex result of dividing this Gint by other"""
+        return complex(self) / complex(other)
+
+    def __floordiv__(self, other):  # self // other
+        """Return the quotient, self / other, using the division theorem
+        """
+        if isinstance(other, int) or isinstance(other, float):
+            q = Gint(complex(self) / other)
+        else:
+            q = Gint(complex(self * other.conj) / other.norm)
+        return q
+
+    def division_theorem(self, other):
+        """Let a = self & b = other, then compute q & r, such that a = b * q + r,
+        and return q & r (i.e., the quotient and remainder of a/b)
+        """
+        q = Gint(complex(self * other.conj) / other.norm)  # Gint rounds the complex result here
+        r = self - other * q
+        return q, r
+
     @classmethod
     def eye(cls):
         """Returns i = Gint(0, 1)"""
@@ -131,44 +142,11 @@ class Gint():
         return Gint(self.real, - self.imag)
 
     @property
-    def norm(self):
+    def norm(self) -> int:
         """Returns the norm of this Gaussian integer.
         NOTE: The norm here is the square of the usual absolute value."""
         n = self * self.conj
         return n.real
-
-    def divides(self, other):
-        """Return True if this Gint divides the other Gint,
-        otherwise return False.
-        """
-        a = self.norm
-        b = other.norm
-        if b % a == 0:
-            return True
-        else:
-            return False
-
-    def divided_by(self, other):
-        """Return True if the other Gint or int divides this Gint,
-        otherwise return False.
-        """
-        if isinstance(other, Gint):
-            return other.divides(self)
-        elif isinstance(other, int):
-            if (self.real % other == 0) and (self.imag % other == 0):
-                # return Gint(self.real // other, self.imag // other)
-                return True
-            else:
-                return False
-        else:
-            raise ValueError(f"{other} is not a valid input")
-
-    def division(self, b):
-        """Compute q & r, such that self = bq + r
-        """
-        q = Gint(complex(self * b.conj) / b.norm)  # Gint rounds the complex parts here
-        r = self - b * q
-        return q, r
 
     def associates(self):
         """Return the list of this Gint's associates"""
@@ -179,7 +157,7 @@ class Gint():
         """Return True if the other Gint is an associate of this Gint,
         otherwise return False.
         """
-        q = self / other
+        q = self // other
         if q:
             if q in Gint.units():
                 return True
@@ -187,3 +165,44 @@ class Gint():
                 return False
         else:
             return False
+
+    # def __truediv__(self, other):
+    #     """If other divides self, then self / other is returned,
+    #     otherwise False is returned.
+    #     """
+    #     if isinstance(other, int):
+    #         oth = Gint(other, 0)
+    #     else:
+    #         oth = other
+    #     numer = self * oth.conj
+    #     denom = oth.norm
+    #     if my.divides(denom, numer.real) and my.divides(denom, numer.imag):
+    #         return Gint(numer.real // denom, numer.imag // denom)
+    #     else:
+    #         return False
+
+    # def divides(self, other):
+    #     """Return True if this Gint divides the other Gint,
+    #     otherwise return False.
+    #     """
+    #     a = self.norm
+    #     b = other.norm
+    #     if b % a == 0:
+    #         return True
+    #     else:
+    #         return False
+
+    # def divided_by(self, other):
+    #     """Return True if the other Gint or int divides this Gint,
+    #     otherwise return False.
+    #     """
+    #     if isinstance(other, Gint):
+    #         return other.divides(self)
+    #     elif isinstance(other, int):
+    #         if (self.real % other == 0) and (self.imag % other == 0):
+    #             # return Gint(self.real // other, self.imag // other)
+    #             return True
+    #         else:
+    #             return False
+    #     else:
+    #         raise ValueError(f"{other} is not a valid input")

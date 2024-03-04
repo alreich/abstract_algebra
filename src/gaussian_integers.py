bab6da547bca03566148285fea5bb977b6ebaa96
Al Reich <al.reich@gmail.com>
2024-03-04 14:27:38 -0600

    updated gaussian integers (Done?)

--- src/gaussian_integers.py
+++ src/gaussian_integers.py
@@ -155,7 +155,7 @@ class Zi(Complex):
         else:
             raise TypeError(f"Multiplication by '{other}' not supported")
         # (a, b) * (c, d) = (a*c - b*d) + (a*d + b*c)
-        if d == 0:  # might as well take advantage of this here
+        if d == 0:  # take advantage of this case to do less computing
             return Zi(a * c, b * c)
         else:
             return Zi(a * c - b * d, a * d + b * c)
@@ -189,16 +189,30 @@ class Zi(Complex):
         else:
             return Zi(a * c - b * d, a * d + b * c)
 
+    # def __pow__(self, n: int, modulo=None):  # self ** n
+    #     result = self
+    #     if isinstance(n, int) and n >= 0:
+    #         if n == 0:
+    #             result = Zi(1)  # Return "1"
+    #         else:
+    #             for _ in range(n - 1):
+    #                 result = result * self
+    #     else:
+    #         raise TypeError(f"The power, {n}, is not a positive integer.")
+    #     return result
+
     def __pow__(self, n: int, modulo=None):  # self ** n
         result = self
-        if isinstance(n, int) and n >= 0:
+        if isinstance(n, int):
             if n == 0:
                 result = Zi(1)  # Return "1"
-            else:
+            elif n > 0:
                 for _ in range(n - 1):
                     result = result * self
+            else:  # n < 0
+                return 1 / (self ** abs(n))
         else:
-            raise TypeError(f"The power, {n}, is not a positive integer.")
+            raise TypeError(f"The power, {n}, must be an integer.")
         return result
 
     def __complex__(self) -> complex:
@@ -223,34 +237,54 @@ class Zi(Complex):
         else:
             return True
 
+    # def __truediv__(self, other) -> Qi:  # self / other
+    #     """Divide self by other, exactly, and return the resulting Gaussian rational, Qi.
+    #
+    #     Implements the / operator, and returns the exact, Gaussian rational result
+    #     of dividing this Gaussian integer by another Gaussian integer or regular integer.
+    #     """
+    #     if isinstance(other, Zi):
+    #         denom = other.norm
+    #         numer = self * other.conjugate
+    #     elif isinstance(other, int):
+    #         denom = other
+    #         numer = self
+    #     else:
+    #         raise TypeError(f"{other} cannot divide a Gaussian integer")
+    #     return Qi(Fraction(numer.real, denom), Fraction(numer.imag, denom))
+
     def __truediv__(self, other) -> Qi:  # self / other
         """Divide self by other, exactly, and return the resulting Gaussian rational, Qi.
 
         Implements the / operator, and returns the exact, Gaussian rational result
-        of dividing this Gaussian integer by another Gaussian integer or regular integer.
+        of dividing this Gaussian integer by another Gaussian integer, or an int,
+        float, or complex. Floats & complexes will be rounded.
         """
-        if isinstance(other, Zi):
-            denom = other.norm
-            numer = self * other.conjugate
-        elif isinstance(other, int):
-            denom = other
-            numer = self
+        if isinstance(other, (int, float, complex)):
+            oth = Zi(other)
+        elif isinstance(other, Zi):
+            oth = other
         else:
-            raise TypeError(f"{other} cannot divide a Gaussian integer")
+            raise TypeError(f"Cannot divide a Gaussian integer by {other}")
+        numer = self * oth.conjugate
+        denom = oth.norm
         return Qi(Fraction(numer.real, denom), Fraction(numer.imag, denom))
 
-    # TODO: Finish implementing this
     def __rtruediv__(self, other):  # other / self
-        """Divide other by self, exactly, and return the resulting complex number.
+        """Divide other by self, exactly, and return the resulting Gaussian rational, Qi.
 
         Implements the 'swapped' version of the / operator, and returns the exact,
-        complex result of dividing other by this Gaussian integer. Other must be
-        a Gaussian integer or a regular integer.
+        Gaussian rational result of dividing other by this Gaussian integer. Other
+        must be a Gaussian integer or an int, float, or complex. Floats & complexes
+        will be rounded.
         """
         if isinstance(other, (int, float, complex)):  # the Zi case is handled by __truediv__
-            return complex(other) / complex(self)
+            oth = Zi(other)
+            numer = oth * self.conjugate
+            denom = self.norm
+            return Qi(Fraction(numer.real, denom), Fraction(numer.imag, denom))
         else:
-            raise TypeError(f"{other} cannot be divided by a Zi")
+            raise TypeError(f"{other} cannot be divided by a Gaussian integer")
 
     def __floordiv__(self, other):  # self // other
         """Implements the // operator using 'round', instead of 'floor'.
@@ -275,7 +309,7 @@ class Zi(Complex):
 
         Returns the remainder of the result from mod_divmod
         """
-        _, r = mod_divmod(self, other)
+        _, r = Zi.mod_divmod(self, other)
         return r
 
     def __hash__(self):
@@ -289,18 +323,18 @@ class Zi(Complex):
     def __pos__(self):
         return +self
 
-    def __rpow__(self):
+    def __rpow__(self, base):
         return NotImplemented
 
-    @classmethod
-    def eye(cls):
+    @staticmethod
+    def eye():
         """Return i = Zi(0, 1)"""
         return Zi(0, 1)
 
-    @classmethod
-    def units(cls):
+    @staticmethod
+    def units():
         """Returns the list of four units, [1, -1, i, -i], as Zis."""
-        return [Zi(1), -Zi(1), cls.eye(), -cls.eye()]
+        return [Zi(1), -Zi(1), Zi.eye(), -Zi.eye()]
 
     @property
     def conjugate(self):
@@ -339,105 +373,103 @@ class Zi(Complex):
         """Convert this to a Gaussian rational."""
         return Qi(self.real, self.imag)
 
-    # def unpack(self):
-    #     """Return the two components of the Zi."""
-    #     return self.real, self.imag
+    # See https://kconrad.math.uconn.edu/blurbs/ugradnumthy/Zinotes.pdf
+    @staticmethod
+    def mod_divmod(a, b):
+        """A modified divmod algorithm for Gaussian integers.
 
+        Returns q & r, such that a = b * q + r, where
+        (1/2) * r.norm < b.norm. This is the Modified Division
+        Theorem described in 'The Gaussian Integers' by Keith Conrad
+        """
+        q = Zi(complex(a * b.conjugate) / b.norm)  # Zi rounds the complex result here
+        r = a - b * q
+        return q, r
 
-# See https://kconrad.math.uconn.edu/blurbs/ugradnumthy/Zinotes.pdf
-def mod_divmod(a: Zi, b: Zi):
-    """A modified divmod algorithm for Gaussian integers.
+    @staticmethod
+    def gcd(a, b, verbose=False):
+        """A gcd algorithm for Gaussian integers.
+        Returns the greatest common divisor of a & b.
 
-    Returns q & r, such that a = b * q + r, where
-    (1/2) * r.norm < b.norm. This is the Modified Division
-    Theorem described in 'The Gaussian Integers' by Keith Conrad
-    """
-    q = Zi(complex(a * b.conjugate) / b.norm)  # Zi rounds the complex result here
-    r = a - b * q
-    return q, r
-
-
-def gcd(a: Zi, b: Zi, verbose=False) -> Zi:
-    """Return the greatest common divisor of self and other.
-
-    This function implements the Euclidean algorithm for Gaussian integers.
-    """
-    zero = Zi()
-    if a * b == zero:
-        raise ValueError(f"Both inputs must be non-zero: {a} and {b}")
-    else:
-        r1, r2 = a, b
-        while r2 != zero:
-            r0, r1 = r1, r2
-            q, r2 = mod_divmod(r0, r1)  # q is not used in the computation
-            if verbose:
-                print(f"   {r0} = {r1} * {q} + {r2}")
-    return r1
-
-
-def xgcd(alpha: Zi, beta: Zi):
-    """The Extended Euclidean Algorithm for Gaussian Integers.
-
-    Three values are returned: a, x, & y, such that
-    the Greatest Common Divisor (gcd) of a & b can be
-    written as gcd = a * x + b * y. x & y are called
-    Bézout's coefficients.
-    """
-    if isinstance(alpha, Zi) and isinstance(beta, Zi):
+        This function implements the Euclidean algorithm for Gaussian integers.
+        """
         zero = Zi()
-    else:
-        raise ValueError(f"Inputs must be two Zis.")
+        if a * b == zero:
+            raise ValueError(f"Both inputs must be non-zero: {a} and {b}")
+        else:
+            r1, r2 = a, b
+            while r2 != zero:
+                r0, r1 = r1, r2
+                q, r2 = Zi.mod_divmod(r0, r1)  # q only used in call to print, below
+                if verbose:
+                    print(f"   {r0} = {r1} * {q} + {r2}")
+        return r1
 
-    # NOTE: Many of the lines below perform two assigment operations
-    a, b = alpha, beta
-    x, next_x = 1, 0
-    y, next_y = 0, 1
-    while b != zero:
-        q = a // b
-        next_x, x = x - q * next_x, next_x
-        next_y, y = y - q * next_y, next_y
-        a, b = b, a % b
-    return a, x, y
+    @staticmethod
+    def xgcd(alpha, beta):
+        """The Extended Euclidean Algorithm for Gaussian Integers.
 
+        Three values are returned: a, x, & y, such that
+        the Greatest Common Divisor (gcd) of a & b can be
+        written as gcd = a * x + b * y. x & y are called
+        Bézout's coefficients.
+        """
+        if isinstance(alpha, Zi) and isinstance(beta, Zi):
+            zero = Zi()
+        else:
+            raise ValueError(f"Inputs must be two Zis.")
 
-def is_gaussian_prime(x: (int, Zi)) -> bool:
-    """Return True if x is a Gaussian prime.  Otherwise, return False.
+        # NOTE: Many of the lines below perform two assigment operations
+        a, b = alpha, beta
+        x, next_x = 1, 0
+        y, next_y = 0, 1
+        while b != zero:
+            q = a // b
+            next_x, x = x - q * next_x, next_x
+            next_y, y = y - q * next_y, next_y
+            a, b = b, a % b
+        return a, x, y
 
-    See https://mathworld.wolfram.com/GaussianPrime.html
-    """
-    re = im = norm = None  # So PyCharm won't complain about using variables before assigning them
+    @staticmethod
+    def is_gaussian_prime(x) -> bool:
+        """Return True if x is a Gaussian prime.  Otherwise, return False.
+        x can be an integer or a Gaussian integer.
 
-    if isinstance(x, Zi):
-        re = abs(x.real)
-        im = abs(x.imag)
-        norm = x.norm
-    elif isinstance(x, int):
-        re = abs(x)
-        im = 0
-        norm = re * re
+        See https://mathworld.wolfram.com/GaussianPrime.html
+        """
+        re = im = norm = None  # So PyCharm won't complain about using variables before assigning them
 
-    if (re * im != 0) and isprime(norm):
-        return True
+        if isinstance(x, Zi):
+            re = abs(x.real)
+            im = abs(x.imag)
+            norm = x.norm
+        elif isinstance(x, int):
+            re = abs(x)
+            im = 0
+            norm = re * re
 
-    elif re == 0:
-        if isprime(im) and (im % 4 == 3):
+        if (re * im != 0) and isprime(norm):
             return True
+
+        elif re == 0:
+            if isprime(im) and (im % 4 == 3):
+                return True
+            else:
+                return False
+
+        elif im == 0:
+            if isprime(re) and (re % 4 == 3):
+                return True
+            else:
+                return False
+
         else:
             return False
-
-    elif im == 0:
-        if isprime(re) and (re % 4 == 3):
-            return True
-        else:
-            return False
-
-    else:
-        return False
 
 
 def isprime(n: int) -> bool:
     """Returns True if n is a positive, prime integer; otherwise, False is returned.
-    The same function exists in SymPy."""
+    FYI, the same function exists in SymPy."""
     if isinstance(n, int):
         if n == 2:
             return True
